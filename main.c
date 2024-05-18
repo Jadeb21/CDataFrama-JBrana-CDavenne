66b7e1a06d17230faf347d362a956196025aee98
Jadeb21 <jade.brana@efrei.net>
2024-05-18 18:43:31 +0200

    commit

--- main.c
+++ main.c
@@ -10,5 +10,9 @@ int main() {
         printf("Value added successfully to my column\n");
     else
         printf("Error adding value to my column\n");
+    insert_value(mycol, 52);
+    insert_value(mycol, 44);
+    insert_value(mycol, 15);
+    print_col(mycol);
     return 0;
 }
